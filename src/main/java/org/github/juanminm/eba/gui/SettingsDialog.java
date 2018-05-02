2822db5f00c77089c81f703292d7f192d8179e76
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2018-05-02 17:00:13 +0200

    Rearranged code and converted local vars to fields

--- src/main/java/org/github/juanminm/eba/gui/SettingsDialog.java
+++ src/main/java/org/github/juanminm/eba/gui/SettingsDialog.java
@@ -12,6 +12,12 @@ public class SettingsDialog extends JDialog {
 
     private static final long serialVersionUID = 1L;
 
+    private JTabbedPane settingTabPane;
+    private JPanel buttonPane;
+    private JButton okButton;
+    private JButton cancelButton;
+    private JButton applyBtn;
+
     /**
      * Launch the application.
      */
@@ -31,40 +37,35 @@ public class SettingsDialog extends JDialog {
     public SettingsDialog() {
         setBounds(100, 100, 450, 300);
         getContentPane().setLayout(new BorderLayout());
-        {
-            JTabbedPane settingTabPane = new JTabbedPane(JTabbedPane.LEFT);
-            getContentPane().add(settingTabPane, BorderLayout.CENTER);
-        }
-        {
-            JPanel buttonPane = new JPanel();
-            buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-            getContentPane().add(buttonPane, BorderLayout.SOUTH);
-            {
-                JButton okButton = new JButton("OK");
-                okButton.addActionListener(e -> {
-                    // TODO Save settings
-                    dispose();
-                });
-                okButton.setActionCommand("OK");
-                buttonPane.add(okButton);
-                getRootPane().setDefaultButton(okButton);
-            }
-            {
-                JButton cancelButton = new JButton("Cancel");
-                cancelButton.setActionCommand("Cancel");
-                cancelButton.addActionListener(e -> dispose());
-                buttonPane.add(cancelButton);
-            }
-            {
-                JButton applyBtn = new JButton("Apply");
-                applyBtn.setEnabled(false);
-                applyBtn.addActionListener(e -> {
-                    saveSettings();
-                    ((JButton) e.getSource()).setEnabled(false);
-                });
-                buttonPane.add(applyBtn);
-            }
-        }
+
+        settingTabPane = new JTabbedPane(JTabbedPane.LEFT);
+        getContentPane().add(settingTabPane, BorderLayout.CENTER);
+
+        buttonPane = new JPanel();
+        buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        getContentPane().add(buttonPane, BorderLayout.SOUTH);
+
+        okButton = new JButton("OK");
+        okButton.addActionListener(e -> {
+            // TODO Save settings
+            dispose();
+        });
+        okButton.setActionCommand("OK");
+        buttonPane.add(okButton);
+        getRootPane().setDefaultButton(okButton);
+
+        cancelButton = new JButton("Cancel");
+        cancelButton.setActionCommand("Cancel");
+        cancelButton.addActionListener(e -> dispose());
+        buttonPane.add(cancelButton);
+
+        applyBtn = new JButton("Apply");
+        applyBtn.setEnabled(false);
+        applyBtn.addActionListener(e -> {
+            saveSettings();
+            ((JButton) e.getSource()).setEnabled(false);
+        });
+        buttonPane.add(applyBtn);
     }
 
     private void saveSettings() {
